bb4f9075eb8ea5d70777b31a622b09f99d553109
Marcin Lemiesz <mar.lemiesz@gmail.com>
2020-02-18 20:00:20 +0100

    Upload file

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -8,6 +8,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Request\ParamFetcher;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ListController extends AbstractFOSRestController
@@ -29,7 +31,7 @@ class ListController extends AbstractFOSRestController
 
     public function getListAction(int $id)
     {
-        $data = $this->taskListRepository->findOneBy(['id'=>$id]);
+        $data = $this->taskListRepository->findOneBy(['id' => $id]);
 
         return $this->view($data, Response::HTTP_OK);
     }
@@ -40,7 +42,7 @@ class ListController extends AbstractFOSRestController
     public function postListsAction(ParamFetcher $paramFetcher)
     {
         $title = $paramFetcher->get('title');
-        if($title){
+        if ($title) {
             $list = new TaskList();
 
             $list->setTitle($title);
@@ -51,8 +53,7 @@ class ListController extends AbstractFOSRestController
             return $this->view($list, Response::HTTP_OK);
         }
 
-        return $this->view(['title'=>'This cannot be null'], Response::HTTP_BAD_REQUEST);
-        
+        return $this->view(['title' => 'This cannot be null'], Response::HTTP_BAD_REQUEST);
     }
 
     public function getListTasksAction(int $id)
@@ -65,5 +66,49 @@ class ListController extends AbstractFOSRestController
 
     public function stateListsAction($id)
     {
+    }
+
+    /**
+     * @Rest\FileParam(name="image", description="The background of the list", nullable=false, image=true)
+     * @param int $id
+     */
+    public function backgroundListAction(Request $request, ParamFetcher $paramFetcher, $id)
+    {
+        $list = $this->taskListRepository->findOneBy(['id'=>$id]);
+        
+        $currentBackground = $list->getBackground();
+        if (!is_null($currentBackground)) {
+            $filesystem = new Filesystem();
+            $filesystem->remove(
+                $this->getUploadDir() . $currentBackground
+            );
+        }
+        $file  = $paramFetcher->get('image');
+        if ($file) {
+            $filename = md5(uniqid()) . '.' . $file->guessClientExtension();
+
+            $file->move(
+                $this->getUploadDir(),
+                $filename
+            );
+
+            $list->setBackground($filename);
+            $list->setBackgroundPath('/uploads/'.$filename);
+
+            $this->entityManager->persist($list);
+            $this->entityManager->flush();
+
+            $data = $request->getUrlForPath(
+                $list->getBackgroundPath()
+            );
+
+            return $this->view($data, Response::HTTP_OK);
+        }
+        return $this->view(['message' => 'Someting went wrong'], Response::HTTP_BAD_REQUEST);
+    }
+
+    private function getUploadDir()
+    {
+        return $this->getParameter('uploads_dir');
     }
 }
