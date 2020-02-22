1097785925a6aded1d5756294d749841fe232efb
Marcin Lemiesz <mar.lemiesz@gmail.com>
2020-02-22 18:06:54 +0100

    Set token to the Cookie

---
+++ src/Listeners/AuthenticationSuccessListener.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Listeners;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\HttpFoundation\Cookie;
+
+class AuthenticationSuccessListener
+{
+
+    private $secure = false;
+    private $tokenTtl;
+
+    /**
+     * AuthenticationSuccessListener constructor.
+     * @param $tokenTtl
+     */
+    public function __construct($tokenTtl)
+    {
+
+        $this->tokenTtl = $tokenTtl;
+    }
+
+    /**
+     * @param AuthenticationSuccessEvent $event
+     * @throws \Exception
+     */
+    public function onAuthenticationSuccess(AuthenticationSuccessEvent $event)
+    {
+        $response = $event->getResponse();
+        $data = $event->getData();
+
+        $token = $data['token'];
+
+        $event->setData([]);
+
+        $response->headers->setCookie(
+            new Cookie(
+                "BEARER",
+                $token,
+                (new \DateTime())->add(new \DateInterval('PT' . 3360 . 'S')),
+                '/',
+                null,
+                $this->secure
+            )
+        );
+    }
+
+}
